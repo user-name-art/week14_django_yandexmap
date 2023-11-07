7887454cc5111fbc1f646128ef41be03db8d4a67
Art Gilyazov <art.gilyazov@mail.ru>
2023-11-07 15:16:26 +0300

    move data from template to view

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,9 +1,37 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
+
+from places.models import Location
 
 
 def index(request):
-    template = loader.get_template('index.html')
-    context = {}
-    rendered_page = template.render(context, request)
-    return HttpResponse(rendered_page)
+    all_locations = Location.objects.all()
+
+    locations_on_map = []
+
+    for location in all_locations:
+        location_properties = {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [location.lng, location.lat]
+          },
+          "properties": {
+            "title": location.title,
+            "placeId": location.title,
+            "detailsUrl": ""
+          }
+        }
+
+        locations_on_map.append(location_properties)
+
+
+    data = {'places':
+        {
+        "type": "FeatureCollection",
+        "features": locations_on_map,
+        }
+    }
+
+    return render(request, "index.html", context=data)
