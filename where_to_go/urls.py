989a891480370218d990f0699ac6a113f9989eee
Art Gilyazov <art.gilyazov@mail.ru>
2023-11-07 16:52:18 +0300

    add JSON API as server response

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -24,6 +24,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
+    path('places/<int:location_id>/', views.get_location),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from places.models import Location
 
@@ -35,3 +35,20 @@ def index(request):
     }
 
     return render(request, "index.html", context=data)
+
+
+def get_location(request, location_id):
+    target_location = get_object_or_404(Location, id=location_id)
+
+    location = {
+        "title": target_location.title,
+        "imgs": [image.photo.url for image in target_location.images.all()],
+        "description_short": target_location.description_short,
+        "description_long": target_location.description_long,
+        "coordinates": {
+          "lat": target_location.lat,
+          "lng": target_location.lng
+        }
+    }
+
+    return JsonResponse(location, safe=False, json_dumps_params={'ensure_ascii': False, 'indent': 2})
