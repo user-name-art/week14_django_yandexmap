14212b3e98b30e57bfff0d1f52ac6f0e9a568be3
Art Gilyazov <art.gilyazov@mail.ru>
2023-11-07 11:46:52 +0300

    add location model

--- places/models.py
+++ places/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
 
-print("hello!")
+class Location(models.Model):
+    title = models.CharField(max_length=200)
+    description_short = models.TextField()
+    description_long = models.TextField()
+    lat = models.FloatField(verbose_name='широта')
+    lng = models.FloatField(verbose_name='долгота')
+
+    def __str__(self):
+        return self.title
