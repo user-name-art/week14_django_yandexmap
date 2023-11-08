e3bc31d53b0e3c713656d945cdb63b18719e9f88
Art Gilyazov <art.gilyazov@mail.ru>
2023-11-08 06:28:57 +0300

    add photo on location page in admin panel

--- places/admin.py
+++ places/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from .models import Location, Image
 
 
-admin.site.register(Location)
+class ImageInline(admin.TabularInline):
+    model = Image
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    inlines = [ImageInline]
+
 admin.site.register(Image)
