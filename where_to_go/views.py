c0bba3a47b50f5d904b696d506cd4e5613977392
Art Gilyazov <art.gilyazov@mail.ru>
2023-11-08 06:08:19 +0300

    using data from the database

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -20,14 +20,14 @@ def index(request):
           "properties": {
             "title": location.title,
             "placeId": location.title,
-            "detailsUrl": ""
+            "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/moscow_legends.json"
           }
         }
 
         locations_on_map.append(location_properties)
 
 
-    data = {'places':
+    data = {"places":
         {
         "type": "FeatureCollection",
         "features": locations_on_map,
@@ -52,3 +52,39 @@ def get_location(request, location_id):
     }
 
     return JsonResponse(location, safe=False, json_dumps_params={'ensure_ascii': False, 'indent': 2})
+
+
+def show_phones(request):
+    all_locations = Location.objects.all()
+
+    locations_on_map = []
+
+    for location in all_locations:
+        location_properties = {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [location.lng, location.lat]
+          },
+          "properties": {
+            "title": location.title,
+            "placeId": location.title,
+            "detailsUrl": "roofs24.json"
+          }
+        }
+
+        locations_on_map.append(location_properties)
+
+
+    context = {"places":
+        {
+        "type": "FeatureCollection",
+        "features": locations_on_map,
+        }
+    }
+
+
+
+    template = loader.get_template('location.html')
+    rendered_page = template.render(context, request)
+    return HttpResponse(rendered_page)
