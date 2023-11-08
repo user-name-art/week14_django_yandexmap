b0019c753aa5d29fc61d04b86b18fa2bbdc910da
Art Gilyazov <art.gilyazov@mail.ru>
2023-11-08 06:12:48 +0300

    removed unnecessary function

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -52,39 +52,3 @@ def get_location(request, location_id):
     }
 
     return JsonResponse(location, safe=False, json_dumps_params={'ensure_ascii': False, 'indent': 2})
-
-
-def show_phones(request):
-    all_locations = Location.objects.all()
-
-    locations_on_map = []
-
-    for location in all_locations:
-        location_properties = {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [location.lng, location.lat]
-          },
-          "properties": {
-            "title": location.title,
-            "placeId": location.title,
-            "detailsUrl": "roofs24.json"
-          }
-        }
-
-        locations_on_map.append(location_properties)
-
-
-    context = {"places":
-        {
-        "type": "FeatureCollection",
-        "features": locations_on_map,
-        }
-    }
-
-
-
-    template = loader.get_template('location.html')
-    rendered_page = template.render(context, request)
-    return HttpResponse(rendered_page)
