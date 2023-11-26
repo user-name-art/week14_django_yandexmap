a2ffa519035fe379277eae0180f49974c90c926b
Art Gilyazov <art.gilyazov@mail.ru>
2023-11-26 06:45:22 +0300

    add WYSIWYG editor to admin page

--- places/models.py
+++ places/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from tinymce import models as tinymce_models
 
 
 class Location(models.Model):
     title = models.CharField(max_length=200)
     description_short = models.TextField()
-    description_long = models.TextField()
+    description_long = tinymce_models.HTMLField()
     lat = models.FloatField(verbose_name='широта')
     lng = models.FloatField(verbose_name='долгота')
 
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from where_to_go import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -25,6 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
     path('places/<int:location_id>/', views.get_location),
+    path('tinymce/', include('tinymce.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
