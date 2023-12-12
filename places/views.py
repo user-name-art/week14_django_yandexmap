388e9f52251e9e9c181ca4f5605355807a808550
Art Gilyazov <art.gilyazov@mail.ru>
2023-12-12 07:36:48 +0300

    one style to string quotes

--- places/views.py
+++ places/views.py
@@ -15,42 +15,42 @@ def index(request):
 
     for location in all_locations:
         location_properties = {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [location.lng, location.lat]
+          'type': 'Feature',
+          'geometry': {
+            'type': 'Point',
+            'coordinates': [location.lng, location.lat]
           },
-          "properties": {
-            "title": location.title,
-            "placeId": location.title,
-            "detailsUrl": reverse('get-location', kwargs={'location_id': location.id})
+          'properties': {
+            'title': location.title,
+            'placeId': location.title,
+            'detailsUrl': reverse('get-location', kwargs={'location_id': location.id})
           }
         }
 
         locations_on_map.append(location_properties)
 
 
-    data = {"places":
+    data = {'places':
         {
-        "type": "FeatureCollection",
-        "features": locations_on_map,
+        'type': 'FeatureCollection',
+        'features': locations_on_map,
         }
     }
 
-    return render(request, "index.html", context=data)
+    return render(request, 'index.html', context=data)
 
 
 def get_location(request, location_id):
     target_location = get_object_or_404(Location, id=location_id)
 
     location = {
-        "title": target_location.title,
-        "imgs": [image.photo.url for image in target_location.images.all()],
-        "description_short": target_location.description_short,
-        "description_long": target_location.description_long,
-        "coordinates": {
-          "lat": target_location.lat,
-          "lng": target_location.lng
+        'title': target_location.title,
+        'imgs': [image.photo.url for image in target_location.images.all()],
+        'description_short': target_location.description_short,
+        'description_long': target_location.description_long,
+        'coordinates': {
+          'lat': target_location.lat,
+          'lng': target_location.lng
         }
     }
 
