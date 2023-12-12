cf78d0c07bdeea2416424b2360d301e6a7ce6650
Art Gilyazov <art.gilyazov@mail.ru>
2023-12-12 23:22:36 +0300

    fix image preview in admin

--- places/admin.py
+++ places/admin.py
@@ -1,6 +1,7 @@
 from adminsortable2.admin import (SortableAdminBase, SortableAdminMixin,
                                   SortableStackedInline)
 from django.contrib import admin
+from django.utils.html import format_html
 from django.utils.safestring import mark_safe
 
 from .models import Image, Location
@@ -12,12 +13,8 @@ class ImageInline(SortableStackedInline):
     readonly_fields = ('get_preview',)
     fields = ('photo', 'get_preview', 'position')
 
-    def get_preview(self, obj):
-        return mark_safe('<img src="{url}" height={height} />'.format(
-                url = obj.photo.url,
-                height=200,
-                )
-        )
+    def get_preview(self, image):
+        return format_html('<img src="{}" style="max-height:{}px"/>', mark_safe(image.photo.url), 200)
 
 
 @admin.register(Location)
