fee1de0fca96291fb9c9a087c95ce206ff20e17f
Art Gilyazov <art.gilyazov@mail.ru>
2023-12-12 07:34:17 +0300

    move views to places

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from where_to_go import views
+from places import views
 from django.conf import settings
 from django.conf.urls.static import static
 
